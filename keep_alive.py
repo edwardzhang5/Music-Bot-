9d2f3937966298afaff18425eaf20412220f1ba3
Edward Zhang <edwardzhang5@gmail.com>
2020-12-28 16:10:33 +0000

    code from valentin.b

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template
 from threading import Thread
 
-app = Flask('Web Page')
+app = Flask('Music App', template_folder='templates')
 
 @app.route('/')
 def home():
-    return render_template("homepage.html")
+    return render_template('homepage.html')
 
-@app.route("/Edward")
+@app.route('/edward')
 def Edward():
-  return "Hello World!!1!"
+    return "Hi fans"
+    #return render_template("homepage.html")
+
+
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
+
 
 def keep_alive():
     t = Thread(target=run)
